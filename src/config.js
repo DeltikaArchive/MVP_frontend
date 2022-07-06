2933f6d2137e771172368a182c8ffb96eeb7cb7d
zhaojingjing95 <zhaojingjing9595@gmail.com>
2022-07-06 14:54:43 +0300

    add config

---
+++ src/config.js
@@ -0,0 +1,4 @@
+// export const URLrequests = "https://cors-everywhere.herokuapp.com/http://back-stag-env.us-east-2.elasticbeanstalk.com";
+"http://127.0.0.1:8000";
+export const URLrequests = "http://0.0.0.0:8000";
+export const URLUsersRequests = "http://0.0.0.0:8001";
